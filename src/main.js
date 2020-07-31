48e8c140c1c5498d2bc33fce023fbfeca0f8711f
Alexandre Souza <gerenciaped@volt.ind.br>
2020-07-31 11:05:55 -0300

    corrigido erro com banco de dados remoto

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
@@ -17,17 +17,17 @@ Vue.use(IconsPlugin);
 Vue.use(
   VueAxios,
   axios.create({
-    //baseURL: "http://192.168.9.244:3333/",
-    baseURL: "http://127.0.0.1:3333/",
+    baseURL: "http://192.168.99.10:3333/",
+    //baseURL: "http://127.0.0.1:3333/",
   })
 );
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
